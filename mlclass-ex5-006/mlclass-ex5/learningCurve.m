59915c62e9ec9249f90ce6b5e46fd0e690347219
Jonathan Tumulak <tumulak.jonathan@gmail.com>
2015-04-17 22:38:50 +0800

    added semicolon

--- mlclass-ex5-006/mlclass-ex5/learningCurve.m
+++ mlclass-ex5-006/mlclass-ex5/learningCurve.m
@@ -57,8 +57,8 @@ for i=1:m
   X_train = X(1:i, :);
   y_train = y(1:i);
   theta = trainLinearReg(X_train, y_train, lambda);
-  error_train(i) = linearRegCostFunction(X_train, y_train, theta, 0)
-  error_val(i) = linearRegCostFunction(Xval, yval, theta, 0)
+  error_train(i) = linearRegCostFunction(X_train, y_train, theta, 0);
+  error_val(i) = linearRegCostFunction(Xval, yval, theta, 0);
 end
 
 
